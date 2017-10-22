00addfd6ecdd6328d683f9e6eb4bf3d20931f2f5
Tom Eccles <t@freedommail.info>
2017-10-22 12:53:46 +0100

    server.c code cleanup & commenting

--- src/server.c
+++ src/server.c
@@ -1,6 +1,6 @@
 /*
  * Copyright 2017
- * MIT Licenced
+ * MIT Licensed
  * sockets.c
  * Functions for running the server
  */
@@ -28,30 +28,31 @@ static GQueue *read_buff = NULL;
 static pthread_mutex_t read_buff_mux = PTHREAD_MUTEX_INITIALIZER;
 
 // the listening socket
-static int listen_socket = - 1;
+static int listen_socket = -1;
 
-// sets up an fd for realtime signal IO
+// sets up a fd for realtime signal IO using signal sig, handled by handler
 static bool setup_rt_signal_io(int fd, int sig, void (*handler)(int, siginfo_t *, void *)) {
     // establish signal handler for new connections
     struct sigaction sa;
     sa.sa_sigaction = handler;
-    sa.sa_flags = SA_RESTART | SA_SIGINFO;
-    sigemptyset(&sa.sa_mask);
-    if (-1 == sigaction(sig, &sa, NULL)) {
+    sa.sa_flags = SA_RESTART | SA_SIGINFO; // SA_RESTART: don't break things so much when we interupt them, SA_SIGINFO: be realtime
+    sigemptyset(&sa.sa_mask); // don't mask signals
+    if (-1 == sigaction(sig, &sa, NULL)) { // install new settings for this signal
         return false;
     }
 
-    // set the owner of the socket so that we get the signals
+    // set the owner of the socket to us so that we get the signals
     if (-1 == fcntl(fd, F_SETOWN, getpid())) {
         return false;
     }
 
     // specify our handler instead of the default SIGIO handler
+    // I am not sure why this is needed in addition to setting sa.sa_sigaction
     if (-1 == fcntl(fd, F_SETSIG, sig)) {
         return false;
     }
 
-    // enable signaling
+    // enable signaling & non-blocking IO
     int flags = fcntl(fd, F_GETFL);
     if (-1 == fcntl(fd, F_SETFL, flags | O_ASYNC | O_NONBLOCK)) {
         return false;
@@ -60,50 +61,58 @@ static bool setup_rt_signal_io(int fd, int sig, void (*handler)(int, siginfo_t *
     return true;
 }
 
-// attempt to read a full  json object from a fd
+// attempt to read a full  json object from a fd (defined as "{*}", handling nesting)
 static ReadStatus read_json_object(int fd, GString **out_string) {
-    // assume that we can read the whole thing now
+    // assume that we can read the whole thing at once
 
     GString *string = g_string_new("{");
 
     // the first character must be '{'
     char buf;
     ssize_t num_read = read(fd, &buf, 1);
-    if (1 != num_read) {
+    if (1 != num_read) { // we could not read a whole character
         int e = errno;
         g_string_free(string, true);
 
-        printf("(first) errno=%i num_read=%li\n", e, num_read);
-
+        // if it looks like there just wasn't any data left in the buffer for us to read
         if ((EAGAIN == e) || (EWOULDBLOCK == e) || (0 == num_read)) {
             return END;
         }
         
+        // there was data but an error occured
         return ERROR;
     }
+
+    // the first character must be {
     if (buf != '{') {
         g_string_free(string, true);
-        if (buf == '\n') // so we can telnet in for tests
-            return END;
+        if ((buf == '\n') || (buf == 13)) // ignore newline characters so we can telnet in for testing
+            return read_json_object(fd, out_string);
+
         printf("invalid c=%i\n", (int) buf);
         return ERROR;
     }
 
     // read in the rest
-    unsigned int nest_count = 1;
+    int nest_count = 1;
     while (true) {
+        // try to read 1 character
         num_read = read(fd, &buf, 1);
         if (1 != num_read) {
             int e = errno;
             g_string_free(string, true);
-            if ((EWOULDBLOCK == e) || (0 == num_read)) {
+
+            // if we reached the end of the buffer
+            if ((EAGAIN == e) || (EWOULDBLOCK == e) || (0 == num_read)) {
                 puts("partial packet!");
                 return ERROR;
             } else {
+                // unknown error
                 printf("errno=%i. num_read=%li\n", e, num_read);
                 return ERROR;
             }
         }
+        // we did read in a character successfully
         
         // nesting count
         if (buf == '{')
@@ -172,6 +181,7 @@ static ReadStatus fetch_item(int fd) {
     return SUCCESS;
 }
 
+// thread to read in all of the objects waiting in a buffer
 /* the actual reading is done in its own thread for the following situation:
  *    1. some normal code has read_buff_mux locked
  *    2. io_handler signal tries to lock read_buff_mux
@@ -230,9 +240,9 @@ static void connect_handler(__attribute__ ((unused)) int sig, siginfo_t *si, __a
         return;
 
     setup_rt_signal_io(fd, SIGRTMIN + READ_SIG, io_handler);
-    printf("connect done\n");
 }
 
+// sets up a sockaddr structure for the IPv4 address described in addr e.g. addr="127.0.0.1"
 struct sockaddr *alloc_addr(const char *addr, uint16_t port) {
     if (NULL == addr)
         return NULL;
@@ -277,15 +287,8 @@ bool start_server(const struct sockaddr *addr, socklen_t addrlen) {
         return false;
     }
 
-    // set up signal-driven IO
+    // set up signal-driven IO on the listening socket
     if (!setup_rt_signal_io(listen_socket, SIGRTMIN + CONNECT_SIG, connect_handler)) {
-        close(listen_socket);
-        listen_socket = -1;
-        return false;
-    }
-
-    // begin listening on the socket
-    if (-1 == listen(listen_socket, SOMAXCONN)) {
         close(listen_socket);
         listen_socket = -1;
         return false;
@@ -294,7 +297,15 @@ bool start_server(const struct sockaddr *addr, socklen_t addrlen) {
     // initialise the read buffer
     read_buff = g_queue_new();
 
-    printf("server set up\n");
+    // begin listening on the socket
+    if (-1 == listen(listen_socket, SOMAXCONN)) {
+        close(listen_socket);
+        listen_socket = -1;
+        g_queue_free(read_buff);
+        read_buff = NULL;
+        return false;
+    }
+
     return true;
 }
 
